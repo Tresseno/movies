28e47fb843b0d1c2b60437d95cd12573f2889427
Tarsisbs <tarsisbsaz0911@gmail.com>
2025-03-25 16:40:20 -0300

    update

---
+++ App.js
@@ -0,0 +1,109 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image} from 'react-native';
+import {Feather} from '@expo/vector-icons'
+
+export default function App() {
+  // Array de imagens
+  const imagens = [
+    require('./assets/1.png'),
+    require('./assets/2.png'),
+    require('./assets/3.png'),
+    require('./assets/4.png')
+  ];
+
+  // Escolhe uma imagem aleatória
+  const imagem = imagens[Math.floor(Math.random() * imagens.length)];
+
+  return (
+    <View style={styles.container}>
+
+      <View style={styles.viewHeader}>
+        <TouchableOpacity>
+          <Feather style={{marginLeft: -70}} name='menu' size={36} color='#fff'/>
+        </TouchableOpacity>
+
+        <Text style={styles.textHeader}> TEC FILMES </Text>
+      </View>
+
+      <View style={styles.containerSearch}>
+        <TextInput 
+          style={styles.inputSearch} 
+          placeholder="Digite o Filme que deseja buscar"
+        />
+      </View>
+
+      <Text style={styles.textBanner}> Em Cartaz </Text>
+
+      <Image style={styles.imageBanner} source={imagem} />
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#141a29',
+    alignItems: "center",
+    width: '100%',
+    height: '100%'
+  },
+
+  viewHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 8,
+    marginLeft: 10
+  },
+
+  textHeader: {
+    fontSize: 25,
+    color: "white",
+    fontWeight: "bold",
+    marginRight: 75
+  },
+
+  containerSearch: {
+    flexDirection: "row",
+    width: '90%',
+    backgroundColor: "transparent",
+    marginTop: 30,
+    alignItems: "center",
+    borderRadius: 3
+  },
+
+  inputSearch: {
+    height: 35,
+    width: '100%',
+    marginLeft: 5,
+    borderColor: "white",
+    borderWidth: 3,
+    borderRadius: 20,
+    padding: 25,
+    color: "white",
+    fontSize: 15
+  },
+
+  imageBanner: {
+    width: '70%',
+    height: 450,
+    marginTop: 15,
+    borderRadius: 10
+  },
+
+  textBanner: {
+    fontSize: 30,
+    color: "white",
+    marginTop: 15,
+    marginLeft: -180,
+    fontWeight: 'bold'
+  },
+
+  filmeCartaz: {
+    width: '50%',
+    height: 150,
+    marginTop: 15,
+    borderRadius: 10
+  }
+});
---
+++ movies.js
@@ -0,0 +1,106 @@
+const DATA = [
+    {
+        id:'01',
+        nome:'Deadpool & Wolverine' ,
+        nota:'8,6',
+        imagem:''
+    },
+
+    {
+        id:'02',
+        nome:'Quarteto Fantástico' ,
+        nota:'8,6',
+        imagem:''
+    },
+
+    {
+        id:'03',
+        nome:'A fuga das galinhas' ,
+        nota:'8,6',
+        imagem:''
+    },
+
+    {
+        id:'04',
+        nome:'Deadpool & Wolverine' ,
+        nota:'8,6',
+        imagem:''
+    },
+
+    {
+        id:'05',
+        nome:'Deadpool & Wolverine' ,
+        nota:'8,6',
+        imagem:''
+    },
+
+    {
+        id:'06',
+        nome:'Deadpool & Wolverine' ,
+        nota:'8,6',
+        imagem:''
+    },
+
+    {
+        id:'07',
+        nome:'Deadpool & Wolverine' ,
+        nota:'8,6',
+        imagem:''
+    },
+
+    {
+        id:'08',
+        nome:'Deadpool & Wolverine' ,
+        nota:'8,6',
+        imagem:''
+    },
+
+    {
+        id:'09',
+        nome:'Deadpool & Wolverine' ,
+        nota:'8,6',
+        imagem:''
+    },
+
+    {
+        id:'10',
+        nome:'Deadpool & Wolverine' ,
+        nota:'8,6',
+        imagem:''
+    },
+
+    {
+        id:'11',
+        nome:'Deadpool & Wolverine' ,
+        nota:'8,6',
+        imagem:''
+    },
+
+    {
+        id:'12',
+        nome:'Deadpool & Wolverine' ,
+        nota:'8,6',
+        imagem:''
+    },
+
+    {
+        id:'13',
+        nome:'Deadpool & Wolverine' ,
+        nota:'8,6',
+        imagem:''
+    },
+
+    {
+        id:'14',
+        nome:'Deadpool & Wolverine' ,
+        nota:'8,6',
+        imagem:''
+    },
+
+    {
+        id:'15',
+        nome:'Deadpool & Wolverine' ,
+        nota:'8,6',
+        imagem:''
+    }
+]
