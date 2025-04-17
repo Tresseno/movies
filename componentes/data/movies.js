15ca54bf68aa9e4d0ad4289f7506f16f08b6f3e4
Tresseno <tressenoyasmim8@gmail.com>
2025-04-16 22:39:24 -0300

    Update movies.js

--- componentes/data/movies.js
+++ componentes/data/movies.js
@@ -3,99 +3,99 @@ const DATA =
 [
     {
         id: '01',
-        nome: 'Nome1',
-        nota: '7',
+        nome: 'Interestellar',
+        nota: '10',
         imagem: 'https://i.pinimg.com/736x/e4/be/67/e4be67d314f2596e5b1da336301b974d.jpg'
     },
 
     {
         id: '02',
-        nome: 'Nome2',
-        nota: '7',
+        nome: 'O Estranho Mundo de Jack',
+        nota: '10',
         imagem: 'https://i.pinimg.com/736x/2a/14/3a/2a143a1d1d5f6244f1252341e7fe3600.jpg'
     },
 
     {
         id: '03',
-        nome: 'Nome3',
-        nota: '7',
+        nome: 'A Pequena Sereia',
+        nota: '10',
         imagem: 'https://i.pinimg.com/736x/1e/0d/6e/1e0d6e9781d05a736367dee60d7faf48.jpg'
     },
 
     {
         id: '04',
-        nome: 'Nome4',
-        nota: '7',
+        nome: 'Lilo e Stitch',
+        nota: '10',
         imagem: 'https://i.pinimg.com/736x/d7/48/f4/d748f4dd30b3049b320a8518c1cef40a.jpg'
     },
 
     {
         id: '05',
         nome: 'Nome4',
-        nota: '7',
+        nota: '10',
         imagem: 'https://i.pinimg.com/736x/58/74/d9/5874d9c38b4f5f0d054dadc4f913af77.jpg'
     },
 
     {
         id: '06',
-        nome: 'Nome4',
-        nota: '7',
+        nome: 'Karatê Kid: Lendas',
+        nota: '10',
         imagem: 'https://i.pinimg.com/736x/52/c0/29/52c0293daf815347a3d2088d45aa216d.jpg'
     },
 
     {
         id: '07',
-        nome: 'Nome4',
-        nota: '7',
+        nome: 'As Branquelas',
+        nota: '10',
         imagem: 'https://i.pinimg.com/736x/19/5e/88/195e88d0cdc5cc829a10d1bdb929bfaa.jpg'
     },
 
     {
         id: '08',
-        nome: 'Nome4',
-        nota: '7',
+        nome: 'Conclave',
+        nota: '10',
         imagem: 'https://i.pinimg.com/736x/91/fc/95/91fc95285034053e3dc5c683e56c8ee9.jpg'
     },
 
     {
         id: '09',
-        nome: 'Nome4',
-        nota: '7',
+        nome: 'Parasita',
+        nota: '10',
         imagem: 'https://i.pinimg.com/736x/ab/e3/f3/abe3f3f834ae636672c52490e4d121da.jpg'
     },
 
     {
         id: '10',
-        nome: 'Nome4',
-        nota: '7',
+        nome: 'A Bela e a Fera',
+        nota: '10',
         imagem: 'https://i.pinimg.com/736x/09/92/47/09924707cb35ab68351a86e505b805ef.jpg'
     },
 
     {
         id: '11',
-        nome: 'Nome4',
-        nota: '7',
+        nome: 'Alice no País das Maravihas',
+        nota: '10',
         imagem: 'https://i.pinimg.com/736x/1d/db/f0/1ddbf007e27844a6a1fb6532845fcb74.jpg'
     },
 
     {
         id: '12',
-        nome: 'Nome4',
-        nota: '7',
+        nome: 'Coraline',
+        nota: '10',
         imagem: 'https://i.pinimg.com/736x/ec/ec/4e/ecec4e0f4c24a460a83698bccffadb62.jpg'
     },
 
     {
         id: '13',
-        nome: 'Nome4',
-        nota: '7',
+        nome: 'O Espetacular Homem Aranha',
+        nota: '10',
         imagem: 'https://i.pinimg.com/736x/d8/a4/84/d8a484f2b1ecaf38255daf8c4cad69cb.jpg'
     },
 
     {
         id: '14',
-        nome: 'Nome4',
-        nota: '7',
+        nome: 'Gente Grande',
+        nota: '10',
         imagem: 'https://i.pinimg.com/736x/32/40/e9/3240e953f6d35233668736cd8bb19ab8.jpg'
     },
 ]
